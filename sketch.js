566b360287f23738875598451050ba3c68dc0177
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-06-17 17:43:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var input,heading;
+
+
+function setup() 
+{
+  createCanvas(400, 400);
+  background(178,255,102);
+  input = createInput();
+  input.position(5,60);
+
+  heading = createElement('h4','Enter any alphabet: ' );
+  heading.position(5,20);
+
+  textAlign(CENTER);
+  textSize(50);
+  
+}
+
+function draw() {
+
+  
+}
+
