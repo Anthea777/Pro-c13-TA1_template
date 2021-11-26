d754cc4d3ea1b93d25bfa45b5ff48004c99fc74f
Anthea777 <78027677+Anthea777@users.noreply.github.com>
2021-11-26 20:37:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,27 @@ function setup()
 }
 
 function draw() {
-
+ const value= document.getElementById("vowels").value();
   
+  switch(value){
+    case 'a':
+      console.log("vowel");
+      break;
+      case 'e':
+      console.log("vowel");
+      break;
+       case 'i':
+      console.log("vowel");
+      break;
+       case 'o':
+      console.log("vowel");
+      break;
+       case 'u':
+      console.log("vowel");
+      break;
+    default:
+      console.log("Please enter a character that is a vowel")
+  }
+   
 }
 
